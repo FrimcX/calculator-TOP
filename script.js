a9e7a18d190389b324ba111bf8711610f3c901fe
FrimcX <181182782+FrimcX@users.noreply.github.com>
2025-04-02 12:49:47 -0300

    Add operate() func to select one of the previous basic math func to work

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//Variables
+
+let number1;
+let number2;
+let operator;
+
 function add(a,b) {
     return a+b;
 }
@@ -14,3 +20,11 @@ function divide(a,b) {
     return a/b;
 }
 
+function operate(num1, opera, num2) {
+    switch(opera) {
+        case ('+'): return add(num1,num2);
+        case ('-'): return subtract(num1,num2);
+        case ('x'): return multiply(num1,num2);
+        case ('/'): return divide(num1,num2);
+    }
+}
